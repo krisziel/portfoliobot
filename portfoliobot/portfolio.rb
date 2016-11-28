1aecf0af4dbf51401caa115ca7408281ba9b80f0
Kristian Ziel <kristian.m.ziel@aexp.com>
2016-11-28 13:11:27 -0700

    added some checks/errors for positions and stock symbols

--- portfoliobot/portfolio.rb
+++ portfoliobot/portfolio.rb
@@ -4,7 +4,7 @@ module PortfolioBot
       @user = user
     end
     def positions
-      positions = $db.execute("SELECT * FROM positions WHERE user=?", [@user])
+      positions = $db.execute("SELECT * FROM positions WHERE user=? ORDER BY symbol", [@user])
       if positions.count == 0
         positions = []
       end
@@ -19,32 +19,58 @@ module PortfolioBot
       position_attachments.unshift(position_attachment portfolio_stats)
       return position_attachments
     end
-    def position_attachment stats
+    def position_attachment stats, symbol = false
       current_value = stats[:value]
       cost_basis = stats[:cost]
       positions = stats[:positions]
       value_change = (current_value - cost_basis)
       percent_change = (value_change / cost_basis)*100
       color = PortfolioBot.color_range percent_change
-      cost_basis_text = "#{positions} #{(positions == 1 ? 'position' : 'positions')} with cost basis of #{PortfolioBot.format_currency(cost_basis)}"
+      if symbol
+        cost_basis_text = "#{positions} #{(positions == 1 ? 'position' : 'positions')} in #{symbol} with cost basis of #{PortfolioBot.format_currency(cost_basis)}"
+      else
+        cost_basis_text = "#{positions} #{(positions == 1 ? 'position' : 'positions')} with cost basis of #{PortfolioBot.format_currency(cost_basis)}"
+      end
       current_value_text = "Value: *#{PortfolioBot.format_currency(current_value)} (#{PortfolioBot.format_currency(value_change, true)} // #{percent_change.round(2)}%)*"
       text = "#{cost_basis_text}\n#{current_value_text}"
       attachment = { fallback: text, color: color, text: text, mrkdwn_in: ["text"] }
     end
     def share_positions symbol
+      positions = $db.execute("SELECT * FROM positions WHERE user=? AND symbol=?", [@user, symbol])
+      if positions == 0
+        return "no positions"
+      else
+        position_stats = { cost: 0, value: 0, positions: positions.count }
+        position_attachments = []
+        positions.each do |position|
+          position_data = build_position_attachment(position)
+          portfolio_stats[:cost] += position_data[:stats][:cost]
+          portfolio_stats[:value] += position_data[:stats][:value]
+          position_attachments<<position_data[:attachment]
+        end
+        position_attachments.unshift(position_attachment portfolio_stats)
+      end
     end
     def add_position text
       position_matcher = /\$([a-zA-Z]{1,5}) ([0-9.]*)\@([\$0-9.]*)/
       position_data = text.match(position_matcher)
-      symbol = position_data[1].upcase
+      stock = Stock.new position_data[1].upcase
       shares = position_data[2]
       price = position_data[3]
-      position_data = [symbol, price, shares, @user]
-      $db.execute("INSERT INTO positions (symbol, price, shares, user) VALUES(?, ?, ?, ?)", position_data)
-      return add_position_attachment(position_data)
+      if price <= 0
+        return "Position's share price must be greater than $0.00"
+      elsif shares <= 0
+        return "Position's number of shares must be greater than 0"
+      elsif stock.name == "N/A"
+        return "#{stock.symbol} does not exist"
+      else
+        position_data = [symbol, price, shares, @user]
+        $db.execute("INSERT INTO positions (symbol, price, shares, user) VALUES(?, ?, ?, ?)", position_data)
+        return add_position_attachment(position_data)
+      end
     end
     def build_position_attachment position
-      symbol = position[0]
+      symbol = position[0].upcase
       price = position[1].to_f
       shares = position[2].to_f
       stock = Stock.new symbol
--- portfoliobot/stock.rb
+++ portfoliobot/stock.rb
@@ -3,16 +3,20 @@ module PortfolioBot
     attr_reader :share_data
 
     def initialize symbol
-      YahooFinance::Client.new.quotes([symbol], [:name, :symbol, :last_trade_price, :change, :change_in_percent]).each do |quote|
+      YahooFinance::Client.new.quotes([symbol.upcase], [:name, :symbol, :last_trade_price, :change, :change_in_percent]).each do |quote|
         @share_data = quote
       end
     end
     def attachments
       share_data = @share_data
-      color = PortfolioBot.color_range share_data.change_in_percent.to_f
-      change_text = "#{PortfolioBot.format_currency(share_data.change)} // #{share_data.change_in_percent}"
-      text = "$#{share_data.last_trade_price} (#{change_text})"
-      attachment = { fallback: share_data.name + " - " + text, color: color, title:"<https://finance.yahoo.com/quote/" + share_data.symbol + "|" + share_data.name + " (" + share_data.symbol + ")>", text: text, mrkdwn: true }
+      if share_data.name == "N/A"
+        color = PortfolioBot.color_range share_data.change_in_percent.to_f
+        change_text = "#{PortfolioBot.format_currency(share_data.change)} // #{share_data.change_in_percent}"
+        text = "$#{share_data.last_trade_price} (#{change_text})"
+        attachment = { fallback: "#{share_data.name} - #{text}", color: color, title: "<https://finance.yahoo.com/quote/#{share_data.symbol}|#{share_data.name} (#{share_data.symbol})>", text: text, mrkdwn: true }
+      else
+        attachment = { fallback: "#{share_data.symbol} is not a valid symbol.", text: "#{share_data.symbol} is not a valid symbol.", mrkdwn: true }
+      end
       return [attachment]
     end
   end
